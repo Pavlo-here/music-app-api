e5bc219bc3ee1424073b55213d586dacc4c68e70
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-10-06 18:37:52 +0300

    implement token API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,8 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    def test_create_user_with_email_successful(self):  # check what this test doing
+    # check what this test doing
+    def test_create_user_with_email_successful(self):
         email = "test@example.com"
         password = "testpass123"
         user = get_user_model().objects.create_user(
