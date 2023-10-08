3d9583ef155edfeafccdf7d03d1bc06d4eb90705
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-10-08 13:01:48 +0300

    added TAG model

--- app/core/models.py
+++ app/core/models.py
@@ -47,6 +47,7 @@ class Playlist(models.Model):
     time_minutes = models.IntegerField()
     general_genre = models.CharField(max_length=255, blank=True)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField("Tag")
 
     def __str__(self):
         return self.title
@@ -55,3 +56,10 @@ class Playlist(models.Model):
 class Tag(models.Model):
     """Tag for filtering playlists."""
     name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
