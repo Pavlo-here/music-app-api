84797e5056fcd957a7f4241ba6856e28bfe9a7cb
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-11-08 11:18:04 +0200

    implement playlist filter feature

--- app/playlist/views.py
+++ app/playlist/views.py
@@ -1,6 +1,12 @@
 """
 Views for the playlist APIs
 """
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 from rest_framework import (
     viewsets,
     mixins,
@@ -19,6 +25,22 @@ from core.models import (
 from playlist import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "tags",
+                OpenApiTypes.STR,
+                description="Comma separated list of IDs to filter",
+            ),
+            OpenApiParameter(
+                "songs",
+                OpenApiTypes.STR,
+                description="Comma separated list of IDs to filter",
+            ),
+        ]
+    )
+)
 class PlaylistViewSet(viewsets.ModelViewSet):
     """View for manage playlist APIs."""
     serializer_class = serializers.PlaylistDetailSerializer
@@ -26,9 +48,25 @@ class PlaylistViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integer"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         """Retrieve playlists for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        songs = self.request.query_params.get('songs')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if songs:
+            song_ids = self._params_to_ints(songs)
+            queryset = queryset.filter(songs__id__in=song_ids)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
